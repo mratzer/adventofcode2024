bb9570ea23cac10427b392ce1e44a80f0a83584d
Markus Ratzer <markus.ratzer@mailbox.org>
2024-12-01 11:10:47 +0100

    Day 01 - Clean Up

--- src/main/java/eu/marrat/adventofcode2024/day01/Day01.java
+++ src/main/java/eu/marrat/adventofcode2024/day01/Day01.java
@@ -2,6 +2,7 @@ package eu.marrat.adventofcode2024.day01;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static eu.marrat.adventofcode2024.util.FileUtils.getLines;
@@ -10,39 +11,50 @@ public class Day01 {
 
     public static void main(String[] args) {
 
+        List<String> lines = getLines("day01/input.txt").toList();
 
-        List<String[]> splitted = getLines("day01/input.txt")
-                .map(StringUtils::split)
-                .toList();
+        int[] lefts = new int[lines.size()];
+        int[] rights = new int[lines.size()];
 
-        int[] lefts = new int[splitted.size()];
-        int[] rights = new int[splitted.size()];
+        fillAndSortArrays(lines, lefts, rights);
 
-        int i = 0;
+        int sum1 = 0;
+        int sum2 = 0;
 
-        for (String[] strings : splitted) {
-            lefts[i] = Integer.parseInt(strings[0]);
-            rights[i++] = Integer.parseInt(strings[1]);
+        for (int i = 0; i < lefts.length; i++) {
+            int left = lefts[i];
+            int right = rights[i];
+
+            sum1 += Math.abs(left - right);
+            sum2 += left * countInSortedArray(rights, left);
         }
 
-//        Arrays.sort(lefts);
-//        Arrays.sort(rights);
-
-        int sum = 0;
-
-        for (int left : lefts) {
-            sum += left * count(rights, left);
-        }
-
-        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum2);
     }
 
-    private static int count(int[] arr, int search) {
+    private static void fillAndSortArrays(List<String> lines, int[] lefts, int[] rights) {
+        int i = 0;
+
+        for (String line : lines) {
+            String[] split = StringUtils.split(line);
+
+            lefts[i] = Integer.parseInt(split[0]);
+            rights[i++] = Integer.parseInt(split[1]);
+        }
+
+        Arrays.sort(lefts);
+        Arrays.sort(rights);
+    }
+
+    private static int countInSortedArray(int[] arr, int search) {
         int count = 0;
 
         for (int i : arr) {
             if (i == search) {
                 count++;
+            } else if (i > search) {
+                break;
             }
         }
 
