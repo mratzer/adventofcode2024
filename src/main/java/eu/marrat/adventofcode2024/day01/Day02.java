88781baa398902d8303e1668c6cd7477fb8320f3
Markus Ratzer <markus.ratzer@bearingpoint.com>
2024-12-02 10:37:17 +0100

    Day 02 - Part 2

--- src/main/java/eu/marrat/adventofcode2024/day01/Day02.java
+++ src/main/java/eu/marrat/adventofcode2024/day01/Day02.java
@@ -11,10 +11,31 @@ public class Day02 {
         long count = getLines("day02/input.txt")
                 .map(StringUtils::split)
                 .map(Day02::toIntArray)
-                .filter(Day02::isSave)
+                .filter(Day02::isSaveWithProblemDampener)
                 .count();
 
         System.out.println(count);
+    }
+
+    private static boolean isSaveWithProblemDampener(int[] report) {
+        if (isSave(report)) {
+            return true;
+        }
+
+        // brute force approach, just try every possibility :)
+
+        int[] variant = new int[report.length - 1];
+
+        for (int i = 0; i < report.length; i++) {
+            System.arraycopy(report, 0, variant, 0, i);
+            System.arraycopy(report, i + 1, variant, i, variant.length - i);
+
+            if (isSave(variant)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static boolean isSave(int[] report) {
