45dd63482a9768c28f0aad028d6e293f7bf5669e
Markus Ratzer <markus.ratzer@bearingpoint.com>
2024-12-03 10:48:48 +0100

    Day 03 - Part 2

--- src/main/java/eu/marrat/adventofcode2024/day01/Day03.java
+++ src/main/java/eu/marrat/adventofcode2024/day01/Day03.java
@@ -2,13 +2,17 @@ package eu.marrat.adventofcode2024.day01;
 
 import eu.marrat.adventofcode2024.util.FileUtils;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Day03 {
 
     private static final Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    private static final Pattern pattern2 = Pattern.compile("(do\\(\\))|(don't\\(\\))|(mul\\(\\d{1,3},\\d{1,3}\\))");
 
     public static void main(String[] args) {
         int sum = FileUtils.getLines("day03/input.txt")
@@ -16,6 +20,12 @@ public class Day03 {
                 .sum();
 
         System.out.println(sum);
+
+        List<String> instructions = FileUtils.getLines("day03/input.txt")
+                .flatMap(Day03::getInstructions)
+                .toList();
+
+        System.out.println(work(instructions));
     }
 
     private static IntStream getMultipliers(String line) {
@@ -28,6 +38,38 @@ public class Day03 {
 
                     return x * y;
                 });
+    }
+
+    private static Stream<String> getInstructions(String line) {
+        return pattern2.matcher(line)
+                .results()
+                .flatMap(r -> IntStream.rangeClosed(1, r.groupCount())
+                        .mapToObj(r::group)
+                        .filter(Objects::nonNull));
+    }
+
+    private static int work(List<String> instructions) {
+        boolean dont = false;
+
+        int sum = 0;
+
+        for (String instruction : instructions) {
+            if ("do()".equals(instruction)) {
+                dont = false;
+            } else if ("don't()".equals(instruction)) {
+                dont = true;
+            } else if (!dont) {
+                Matcher matcher = pattern.matcher(instruction);
+                if (matcher.matches()) {
+                    int x = Integer.parseInt(matcher.group(1));
+                    int y = Integer.parseInt(matcher.group(2));
+
+                    sum += x * y;
+                }
+            }
+        }
+
+        return sum;
     }
 
 }
